1ffa39ccfbc66f5d2358bc6eb8c64e42b169a87f
badflockmaster <2061469002@qq.com>
2017-10-04 09:57:04 +0800

    回到顶部函数编写完成

--- top.js
+++ top.js
@@ -1,6 +1,19 @@
 window.onload=function(){
     //获取界面内容的div
     var main = document.getElementById("main");
+    //获取回到顶部的div
+    var top = document.getElementsByClassName("top")[0];
+
+    var timer = null;
+    var flag = true;
+    window.onscroll = function()
+    {
+        if(!flag)
+        {
+            clearInterval(timer);
+        }
+        flag = false;
+    };
     //先初始化界面，让界面height变大
     for(var i = 0; i < 10000; i++)
     {
@@ -9,4 +22,20 @@ window.onload=function(){
         p.appendChild(text); // 将创建的文本节点text添加到p中；
         main.appendChild(p); // 将所有创建的节点添加到main中去；
     }
+
+    //点击按钮回到顶部
+    top.onclick=function()
+    {
+        timer = setInterval(function () {
+            var otop = document.documentElement.scrollTop||document.body.scrollTop;
+            var speed = Math.ceil(otop/6);
+            document.documentElement.scrollTop = document.body.scrollTop -= speed;
+            flag = true;
+            if(otop == 0)
+            {
+                clearInterval(timer);
+            }
+        }, 30);
+        //获取滚动条距离顶部的距离
+    };
 };
